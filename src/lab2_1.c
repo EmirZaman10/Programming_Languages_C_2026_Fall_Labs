33d43312b8c8e83dbda090682ec1f7dd9f8661da
EmirZaman10 <emir201610@gmail.com>
2026-09-21 10:48:07 +0000

    lab2

--- src/lab2_1.c
+++ src/lab2_1.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /*
     Task:
     Write a function `int sum_to_n(int n)` that computes
@@ -10,10 +9,12 @@
       - If n < 1, print an error
       - Otherwise, call sum_to_n and print the result
 */
-
 int sum_to_n(int n) {
-    // TODO: implement sum with a for loop
-    return 0; // placeholder
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
 int main(void) {
@@ -21,8 +22,14 @@ int main(void) {
 
     printf("Enter a positive integer n: ");
     scanf("%d", &n);
-
     // TODO: validate input, call function, and print result
-
+    while (n < 1) {
+        printf("Error: n must be at least 1.\n");
+        scanf("%d", &n);
+    } 
+    
+        int result = sum_to_n(n);
+        printf("Sum from 1 to %d is: %d\n", n, result);
+    
     return 0;
 }
